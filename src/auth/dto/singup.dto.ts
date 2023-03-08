9f99392e5293d3bfcfe16cd1aa35c9f56e31a5fe
tebzi44 <108348938+tebzi44@users.noreply.github.com>
2023-03-08 10:28:40 -0800

    v4.1

--- src/auth/dto/singup.dto.ts
+++ src/auth/dto/singup.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword, MaxLength, MinLength } from 'class-validator';
 
 export class SignupDto {
+    @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(40)
     name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @IsStrongPassword()
     password: string;
 }
